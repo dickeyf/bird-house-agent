42e45870748f71781e7ef31944873389652d92be
Francois Dickey <francois.dickey@solacesystems.com>
2020-01-11 15:11:53 -0500

    Initial commit of the Spring Initializer project

---
+++ src/main/java/com/brewingmadscientists/birdhouseagent/BirdHouseAgentApplication.java
@@ -0,0 +1,13 @@
+package com.brewingmadscientists.birdhouseagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdHouseAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdHouseAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brewingmadscientists/birdhouseagent/BirdHouseAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brewingmadscientists.birdhouseagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdHouseAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
